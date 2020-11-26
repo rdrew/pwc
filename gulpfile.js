c2f71484f886f064564ef654a40bbbeef03f28c9
rdrew <rdrew@upei.ca>
2020-11-26 15:13:19 -0400

    lp work

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 //@format
-var gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  browserSync = require('browser-sync').create(),
-  runSequence = require('run-sequence'),
-  del = require('del'),
-  glob = require('glob'),
-  autoprefixer = require('gulp-autoprefixer'),
-  importer = require('node-sass-globbing'),
-  sourcemaps = require('gulp-sourcemaps'),
-  imagemin = require('gulp-imagemin'),
-  sassLint = require('gulp-sass-lint');
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  browserSync = require("browser-sync").create(),
+  runSequence = require("run-sequence"),
+  del = require("del"),
+  glob = require("glob"),
+  autoprefixer = require("gulp-autoprefixer"),
+  importer = require("node-sass-globbing"),
+  sourcemaps = require("gulp-sourcemaps"),
+  imagemin = require("gulp-imagemin"),
+  sassLint = require("gulp-sass-lint");
 //neat = require('bourbon-neat').includePaths,
 //bourbon = require('bourbon').includePaths;
 
@@ -18,33 +18,33 @@ var gulp = require('gulp'),
 //############################
 
 //name of the drupal theme:
-var _themeName = 'pwc';
+var _themeName = "pwc";
 
 //path to the themes assets (compiled css, js, imgs) dir
 //var _path = "/themes/orion/build";
-var _path = '/sites/pwc.dev.islandarchives.ca/themes/pwc/build';
+var _path = "/sites/pwc.dev.islandarchives.ca/themes/pwc/build";
 
 //make sure the 2 Dirs are correct
 var config = {
   //remoteURL: 'https://dev.islandarchives.ca/',
-  remoteURL: 'https://pwc.dev.islandarchives.ca',
-  srcDir: './src',
-  injectDir: './build',
+  remoteURL: "https://pwc.dev.islandarchives.ca",
+  srcDir: "./src",
+  injectDir: "./build",
   bsPort: 8000,
   //localPath : _path,
   //localPath : "/localpath",
-  localPath: '/build',
+  localPath: "/build",
   //bs find and replace
   remoteCss:
-    '/sites/pwc.dev.islandarchives.ca/themes/pwc/build/css/pwc.styles.css',
-  localCss: '/build/css/pwc.styles.css',
+    "/sites/pwc.dev.islandarchives.ca/themes/pwc/build/css/pwc.styles.css",
+  localCss: "/build/css/pwc.styles.css",
   remoteJs:
-    '/sites/pwc.dev.islandarchives.ca/themes/pwc/build/js/pwc.behaviors.js',
-  localJs: '/build/js/pwc.behaviors.js',
+    "/sites/pwc.dev.islandarchives.ca/themes/pwc/build/js/pwc.behaviors.js",
+  localJs: "/build/js/pwc.behaviors.js",
 
   localAssets: {
-    css: ['css/**/*.css'],
-    js: ['js/**/*.js'],
+    css: ["css/**/*.css"],
+    js: ["js/**/*.js"],
   },
 };
 
@@ -56,19 +56,19 @@ var sass_config = {
     //'node_modules/breakpoint-sass/stylesheets/',
     //'node_modules/singularitygs/stylesheets/',
     //'node_modules/compass-mixins/lib/',
-    //'node_modules/bourbon/app/assets/stylesheets/',
-    //'node_modules/bourbon-neat/app/assets/stylesheets/'
-    'node_modules/foundation-sites/scss',
+    "node_modules/modularscale-sass/stylesheets/",
+    "node_modules/gerillass/scss/",
+    "node_modules/foundation-sites/scss",
   ],
 };
 
-var targetCss = _themeName + '.styles.css';
+var targetCss = _themeName + ".styles.css";
 
 // ##################
 // Clean Task
 // ##################
 
-gulp.task('clean', function() {
+gulp.task("clean", function () {
   return del.sync(config.injectDir);
 });
 
@@ -76,20 +76,20 @@ gulp.task('clean', function() {
 // Sass Development Task
 // ##################
 
-gulp.task('sass_dev', function() {
+gulp.task("sass_dev", function () {
   //this gets stuff in subfolders also and i dont want that atm
   ////return gulp.src(config.srcDir + "/styles/scss/**/*.scss")
   return gulp
-    .src(config.srcDir + '/sass/*.scss')
+    .src(config.srcDir + "/sass/*.scss")
     .pipe(sourcemaps.init())
-    .pipe(sass(sass_config).on('error', sass.logError))
+    .pipe(sass(sass_config).on("error", sass.logError))
     .pipe(
       autoprefixer({
-        browsers: ['last 2 versions', 'ie >= 9', 'and_chr >= 2.3'],
-      }),
+        browsers: ["last 2 versions", "ie >= 9", "and_chr >= 2.3"],
+      })
     )
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(config.injectDir + '/css'))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(config.injectDir + "/css"))
     .pipe(browserSync.stream());
 });
 
@@ -97,20 +97,20 @@ gulp.task('sass_dev', function() {
 // Sass Production Task
 // ##################
 
-gulp.task('sass_prod', function() {
+gulp.task("sass_prod", function () {
   //this gets stuff in subfolders also and i dont want that atm
   ////return gulp.src(config.srcDir + "/styles/scss/**/*.scss")
   return gulp
-    .src(config.srcDir + '/sass/*.scss')
+    .src(config.srcDir + "/sass/*.scss")
     .pipe(sourcemaps.init())
-    .pipe(sass(sass_config).on('error', sass.logError))
+    .pipe(sass(sass_config).on("error", sass.logError))
     .pipe(
       autoprefixer({
-        browsers: ['last 2 versions', 'ie >= 9', 'and_chr >= 2.3'],
-      }),
+        browsers: ["last 2 versions", "ie >= 9", "and_chr >= 2.3"],
+      })
     )
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(config.injectDir + '/css'))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(config.injectDir + "/css"))
     .pipe(browserSync.stream());
 });
 
@@ -118,13 +118,13 @@ gulp.task('sass_prod', function() {
 // Images Task
 // ##################
 
-gulp.task('images', function() {
+gulp.task("images", function () {
   return (
     gulp
-      .src(config.srcDir + '/img/**/*')
+      .src(config.srcDir + "/img/**/*")
       //pipe through image_min
       .pipe(imagemin())
-      .pipe(gulp.dest(config.injectDir + '/img'))
+      .pipe(gulp.dest(config.injectDir + "/img"))
   );
 });
 
@@ -132,10 +132,10 @@ gulp.task('images', function() {
 // JS Task
 // ##################
 
-gulp.task('js', function() {
+gulp.task("js", function () {
   return gulp
-    .src(config.srcDir + '/js/**/*.js')
-    .pipe(gulp.dest(config.injectDir + '/js'))
+    .src(config.srcDir + "/js/**/*.js")
+    .pipe(gulp.dest(config.injectDir + "/js"))
     .pipe(browserSync.stream());
 });
 
@@ -143,18 +143,18 @@ gulp.task('js', function() {
 // Browsersync Task
 // ##################
 
-gulp.task('browserSync', ['sass_dev', 'js'], function() {
+gulp.task("browserSync", ["sass_dev", "js"], function () {
   browserSync.init({
     proxy: config.remoteURL,
     port: config.bsPort,
     //logLevel: 'debug',
-    serveStatic: ['.'],
-    startPath: 'user',
+    serveStatic: ["."],
+    startPath: "user",
     //open: false,
     injectChanges: true,
     //files: ['build/css/islandarchives.styles.css', 'build/js/islandarchives.behaviors.js'],
-    files: ['build/css/*.styles.css', 'build/js/*.behaviors.js'],
-    plugins: ['bs-rewrite-rules'],
+    files: ["build/css/*.styles.css", "build/js/*.behaviors.js"],
+    plugins: ["bs-rewrite-rules"],
     rewriteRules: [
       {
         match: config.remoteCss,
@@ -172,24 +172,24 @@ gulp.task('browserSync', ['sass_dev', 'js'], function() {
 // Watch Task
 // ##################
 
-gulp.task('watch', ['browserSync', 'js', 'images', 'sass_dev'], function() {
-  gulp.watch(config.srcDir + '/sass/**/*.scss', ['sass_dev']);
-  gulp.watch(config.srcDir + '/img/**/*', ['images']);
-  gulp.watch(config.srcDir + '/js/**/*.js', ['js']);
+gulp.task("watch", ["browserSync", "js", "images", "sass_dev"], function () {
+  gulp.watch(config.srcDir + "/sass/**/*.scss", ["sass_dev"]);
+  gulp.watch(config.srcDir + "/img/**/*", ["images"]);
+  gulp.watch(config.srcDir + "/js/**/*.js", ["js"]);
 });
 
 // ##################
 // Build Task
 // ##################
 
-gulp.task('build', function() {
-  runSequence(['clean', 'sass_dev', 'js', 'images']);
+gulp.task("build", function () {
+  runSequence(["clean", "sass_dev", "js", "images"]);
 });
 
 // ##################
 // Default Task
 // ##################
 
-gulp.task('default', function() {
-  runSequence(['build', 'browserSync', 'watch']);
+gulp.task("default", function () {
+  runSequence(["build", "browserSync", "watch"]);
 });
